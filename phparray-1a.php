6af06a3de509555a9030be5f61ff8ec0cb9433fa
MarcusChong123 <kok_yung@hotmail.com>
2018-11-05 23:42:43 +0800

    Update

--- phparray-1a.php
+++ phparray-1a.php
@@ -8,7 +8,9 @@ $a = array(
 $table = '<table border="1" width="100%">';
 $table .= '<tr><th>ID</th><th>Title</th><th>Price</th></tr>';
 foreach($a as $v) {
-    $table .= '<tr><td>'.$v['id'].'</td><td>'.$v['title'].'</td><td>'.$v['price'].'</td></tr>';
+	$table .= '<tr>
+		<td>'.$v['id'].'</td><td>'.$v['title'].'</td><td>'.$v['price'].'</td>
+			</tr>';
 }
 $table .= '</table>';
 
--- table-jason.php
+++ table-jason.php
@@ -1,11 +1,11 @@
 <?php
-require "Conteohtool.php";
+require "Conteohtool.php"; //same as include.php
 $a = array(
 	0 => array("id"=>1,"title"=>"iPhone","price"=>3600),
 	1 => array("id"=>2,"title"=>"Samsung","price"=>2500),
 	2 => array("id"=>3,"title"=>"HTC","price"=>1566),
 );
-$tool = new Conteohtool();
+$tool = new Conteohtool(); //instantiate Jason tool from class to object
 echo $tool->tableGenerator($a);
 
 ?>
